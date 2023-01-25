510ff11017914b47a390131ae3b81dd98a58ca55
John Bjerke <bjerkeweb@gmail.com>
2023-01-25 16:28:18 -0500

    ts ignore

--- src/pages/api/handler.ts
+++ src/pages/api/handler.ts
@@ -9,6 +9,7 @@ const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 
 const twilio_client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 const redis = new Redis({
+  // @ts-ignore
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN
 });
