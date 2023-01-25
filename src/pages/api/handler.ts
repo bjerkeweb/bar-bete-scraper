266ab21023f32d898717cc6b06e386ee7267487b
John Bjerke <bjerkeweb@gmail.com>
2023-01-25 15:44:59 -0500

    first pass at twilio sms integration

--- src/pages/api/handler.ts
+++ src/pages/api/handler.ts
@@ -1,6 +1,12 @@
+import twilio from 'twilio';
 import axios from "axios";
 import cheerio from "cheerio";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const TWILIO_ACCOUNT_SID = 'ACd998ee990bc7bd549b1ee1aa1eeffe24';
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+
+const twilio_client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 const URL = 'https://www.barbete.com/mobile-menu';
 
@@ -8,6 +14,7 @@ const URL = 'https://www.barbete.com/mobile-menu';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
+
     const { data } = await axios.get(URL);
 
     const $ = cheerio.load(data);
@@ -18,7 +25,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       content.push($(el).text())
     });
 
-
     const end = content.findIndex(e => e.includes('Consuming raw or undercooked meats'));
 
     content = content.slice(0, end);
@@ -27,11 +33,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const regex = /(([A-Z]+?[\s,\-,&]+){1,})[a-z,à-ÿ,' ',\d,&,-]+/gm;
 
-    const result = Array.from(str.matchAll(regex), m => m[0]).map(e => e.trim());
+    // iterate over regex match groups, remove consecutive whitespaces
+    const result = Array.from(str.matchAll(regex), m => m[0])
+      .map(e => e.trim())
+      .map(e => e.replace(/\s\s+/g, ' '));
 
-    console.log(result)
+    const message_body = result.join('\n').slice(0, 300);
 
-    res.status(200).json(result);
+    await twilio_client.messages.create({
+      body: message_body,
+      to: '+14152331791',
+      from: '+18558193039'
+    });
+
+    res.status(200).json(message_body);
   } catch (e) {
     throw e;
   }
